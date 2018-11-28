f2563d0e76c97a5ac57bf308486ee9dddd1a1ce7
Neeraj Samtani <neerajsamtani@g.ucla.edu>
2018-11-28 13:44:08 -0800

    Initial commit

---
+++ gif_creator.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import imageio
+import visvis as vv
+import time
+start_time = time.time()
+
+# Return the folder in which the script is stored
+def self_path():
+    path = os.path.dirname(sys.argv[0])
+    if not path:
+        path = '.'
+    return path
+
+# Set the variables to the approproate folders
+current_folder = self_path()
+A = os.path.join(current_folder, 'Images', 'A')
+B = os.path.join(current_folder, 'Images', 'B')
+C = os.path.join(current_folder, 'Images', 'C')
+D = os.path.join(current_folder, 'Images', 'D')
+
+# If an argument is not specified, assume the name of the image is "IMG_0001.jpg"
+if(len(sys.argv) > 1):
+	imagename = sys.argv[1]
+else:
+	imagename = "IMG_0001.jpg"
+
+filenames = [os.path.join(A, imagename), 
+			os.path.join(B, imagename),
+			os.path.join(C, imagename),
+			os.path.join(D, imagename),
+			os.path.join(C, imagename),
+			os.path.join(B, imagename),
+			os.path.join(A, imagename)]
+
+images = []
+for filename in filenames:
+    images.append(imageio.imread(filename))
+imageio.mimsave(os.path.join(current_folder, 'Images', 'Gifs', 'IMG_0001.gif'), images, duration = 0.2)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+k = input("press enter to exit") 
